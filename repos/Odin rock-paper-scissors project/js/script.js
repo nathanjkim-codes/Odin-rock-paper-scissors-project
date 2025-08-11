fa5a29d675eb53d270fa4ac15ac0d01506ef4487
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2025-08-11 12:13:37 -0700

    docs: add pseudocode for rock-paper-scissors project step 1

---
+++ repos/Odin rock-paper-scissors project/js/script.js
@@ -0,0 +1,10 @@
+// Write the logic to get the computer choice
+
+// a. GENERATE random number between 0 and 2 using Math.random
+// b. IF random number is 0 THEN
+//      RETURN "rock"
+//    ELSE IF random number is 1 THEN
+//      RETURN "paper"
+//   ELSE
+//      RETURN "scissors"
+// END FUNCTION
