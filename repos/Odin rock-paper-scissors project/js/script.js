47236768bfb8312ce39efa35b838035bb8b5d686
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2025-08-11 20:37:19 -0700

    feat: implement getComputerChoice function for random rock-paper-scissors choice

--- repos/Odin rock-paper-scissors project/js/script.js
+++ repos/Odin rock-paper-scissors project/js/script.js
@@ -8,3 +8,16 @@
 //   ELSE
 //      RETURN "scissors"
 // END FUNCTION
+
+function getComputerChoice() {
+  let randomNum = Math.floor(Math.random() * 3);
+
+  if (randomNum === 0) {
+    return "rock";
+  } else if (randomNum === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+console.log(getComputerChoice());
